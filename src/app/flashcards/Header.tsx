054662331530201c8a8a74b8ebe58089633e84d2
HarishBalaji ShanmugaSundaram <53060756+HarishBalajiShanmugaSundaramMS@users.noreply.github.com>
2023-10-05 23:29:13 +0200

    minor bug fix

--- src/app/flashcards/Header.tsx
+++ src/app/flashcards/Header.tsx
@@ -5,8 +5,8 @@ import Data from "./Data.json";
 
 function Header() {
   return (
-    <header className="bg-slate-900 py-2 text-center sticky top-0 left-0 w-full z-50">
-      <h1 className="text-yellow-500	uppercase text-2xl font-black hover:animate-jump">
+    <header className="bg-slate-900  text-stone-500 py-2 text-center fixed top-0 left-0 w-full z-50">
+      <h1 className=" hover:text-yellow-500	uppercase text-2xl font-black hover:animate-jump">
         {Object.keys(Data).length + " "}BrainCards
       </h1>
     </header>
